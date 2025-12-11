90261182ead6c39c37ac36ef4a121e74ebc997ec
Todd Demone <tdemone1@gmail.com>
2025-12-11 15:46:51 -0500

    add eslint-config-prettier and eslint-plugin-orettier so eslint and prettier get along; add printWidth: 120 to prettierrc (max line length); add browser to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 
-import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      quotes: ["error", "double"],
+    },
+  },
+  eslintConfigPrettier,
 );
